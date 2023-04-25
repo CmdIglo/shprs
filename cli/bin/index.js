0921e4cb43fce400d4362cf05a586b34ccd7309d
CmdIglo <maxwell.leu@web.de>
2023-04-25 12:14:54 +0200

    npm is the biggest bs

--- cli/bin/index.js
+++ cli/bin/index.js
@@ -1,9 +1,16 @@
 
-const yargs = require('yargs');
+import yargs from 'yargs';
+import chalk from 'chalk';
+import boxen from 'boxen';
+
+const usage = chalk.blue("\nUsage: mycli -l <language>  -s <sentence> \n"
++ boxen(chalk.green("\n" + "Translates a sentence to specific language" + "\n"), {padding: 1, borderColor: 'green', dimBorder: true}) + "\n");
 
 const argv = yargs.options({
     a: {alias: "all", describe: "Parse all shell scripts (current dir)", demandOption: false},
     p: {alias: "path", type: "string", describe: "Set path of shell scripts", demandOption: false, default: "./"}
-}).argv;
+})
+.usage(usage)
+.argv;
 
 console.log(argv);
