000df891ef92d103ce094fda70c908f68772d570
CmdIglo <maxwell.leu@web.de>
2023-05-03 12:35:04 +0200

    Work on parser function

--- cli/parser.c
+++ cli/parser.c
@@ -1,31 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "tokens.h"
 
-int main(int argc, char* argv[]) {
-	char * buffer = 0;
-	int length;
-	FILE* f = fopen(argv[0], "rb");
 
-	TOKEN_NAMES name = TOKEN_OPAREN;
-
-	if(f) {
-		fseek(f, 0, SEEK_END);
-		length = ftell(f);
-		fseek(f, 0, SEEK_SET);
-		buffer = malloc(length);
-		if(buffer){
-			fread(buffer, 1, length, f);
-		}
-		fclose(f);
-	}
-
-	if(buffer) {
-		printf("%.*s", length, buffer);
-	}
+//main function
+int main(int argc, char *argv[]) {
 	
+	//filename as character array (string)
+	char filename[] = "test";
+
+	/*
+	 * Doesnt work yet
+	if(argc > 1) {	
+		filename = "test";
+	} else {
+		printf("No filename provided!");
+		return 1;
+	}
+	*/
+
+	//open file
+	FILE *fptr = fopen(filename, "r");
+
+	//struct that stores metadata about the file
+	struct stat st;
+	//gets the stats for filename and writes it to st
+	stat(filename, &st);
+	//get the size of the file
+	off_t size = st.st_size;
+
+	//print the file size
+	printf("%ld\n", size);
+
+	//refernces the TOKEN_NAMES enum from "tokens.h"
+	TOKEN_NAMES name = TOKEN_OPAREN;
+	
+	//store the value of the comparison to con
 	int con = name == TOKEN_OPAREN;
-	printf("%d", con);
+	//print con
+	printf("%d\n", con);
+
+	return 0;
 
 }
 
